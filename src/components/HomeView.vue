0a3051586f057cdfe94b01e62e5fb0028c6a0e9d
thomas.berlier <thomas.berlier@mirahi.io>
2023-06-16 14:33:14 +0200

    to do list 1

---
+++ src/components/HomeView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+type newTaskType = {
+    name: string,
+    hours: number,
+    done: boolean
+}
+
+const taskList = ref([])
+const newTask = ref('')
+
+const addTask = (task) => {
+  taskList.value.push(task)
+    newTask.value = ""
+}
+</script>
+
+<template>
+  <section class="flex flex-col gap-12">
+    <div class="text-lg flex gap-4 items-center">
+      <input type="text" v-model="newTask" class="text-black" />
+      <button @click="addTask(newTask)" class="">Add</button>
+    </div>
+    <div class="bg-gray-700 p-4">
+      <p v-for="item in taskList" :key="item">
+        {{ item }}
+      </p>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
