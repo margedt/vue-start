96c793cd42f377c57144fdd85925539829b83ecf
thomas.berlier <thomas.berlier@mirahi.io>
2023-06-23 10:38:28 +0200

    to do list 2

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -2,29 +2,33 @@
 import { ref } from 'vue'
 
 type newTaskType = {
-    name: string,
-    hours: number,
-    done: boolean
+  name: string
+  hours: number
+  done: boolean
 }
 
 const taskList = ref([])
-const newTask = ref('')
+const newTask = ref({ name: '', hours: 0, done: false } as newTaskType)
 
-const addTask = (task) => {
-  taskList.value.push(task)
-    newTask.value = ""
+const addTask = () => {
+  taskList.value.push({
+      name: newTask.value.name,
+      hours: newTask.value.hours,
+      done: newTask.value.done,
+  })
 }
 </script>
 
 <template>
   <section class="flex flex-col gap-12">
     <div class="text-lg flex gap-4 items-center">
-      <input type="text" v-model="newTask" class="text-black" />
-      <button @click="addTask(newTask)" class="">Add</button>
+      <input type="text" v-model="newTask.name" class="text-black" placeholder="name" />
+      <input type="number" v-model="newTask.hours" class="text-black" />
+      <button @click="addTask" class="">Add</button>
     </div>
     <div class="bg-gray-700 p-4">
-      <p v-for="item in taskList" :key="item">
-        {{ item }}
+      <p v-for="(item, index) in taskList" :key="index">
+        {{ JSON.stringify(item, null, 2) }}
       </p>
     </div>
   </section>
